6448b37254e226aebe93daaf4209be2017ad5814
Peter P <peter.pistorius@gmail.com>
2016-08-23 08:58:33 +0200

    Update index.js
    From the notes:
    > Note: Although dimensions are available immediately, they may change (e.g due to device rotation) so any rendering logic or styles that depend on these constants should try to call this function on every render,

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ import React, { PropTypes } from 'react';
 import { Dimensions, Text, StyleSheet } from 'react-native';
 
 const flattenStyle = StyleSheet.flatten;
-const { width } = Dimensions.get('window');
 
 
 const ScalableText = ({ style, children }) => {
   const fontSize = flattenStyle(style).fontSize || 14;
+  const { width } = Dimensions.get('window');
   const scaledFontSize = Math.round(fontSize * width / 375);
 
   return <Text style={[style, { fontSize: scaledFontSize }]}>{children}</Text>;
