f54b710fb7298d2defa8f3373897acb08d9d718c
Goran Gajic <fero.null@gmail.com>
2016-08-25 15:25:39 +0200

    use real width

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 import React, { PropTypes } from 'react';
 import { Dimensions, Text, StyleSheet } from 'react-native';
-
+const { width, height } = Dimensions.get('window');
 const flattenStyle = StyleSheet.flatten;
-
+const realWidth = height > width ? width : height;
 
 const ScalableText = ({ style, children, ...props }) => {
   const fontSize = flattenStyle(style).fontSize || 14;
-  const { width } = Dimensions.get('window');
-  const scaledFontSize = Math.round(fontSize * width / 375);
+  const scaledFontSize = Math.round(fontSize * realWidth / 375);
 
   return (
     <Text style={[style, { fontSize: scaledFontSize }]} {...props}>
