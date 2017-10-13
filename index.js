33a26aa56007f431aed543ac1f7d99747ca67c54
Sandro Machado <sandro@uphold.com>
2017-10-13 16:12:05 +0100

    Add support for React 16

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { Dimensions, Text, StyleSheet } from 'react-native';
 const { width, height } = Dimensions.get('window');
 const flattenStyle = StyleSheet.flatten;
